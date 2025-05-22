fc94fe14b2004bde6f4e04393c9c33cb4779cdea
xHarColx <harol@Harol>
2025-05-22 15:42:25 -0500

    miller tarao

--- src/main/java/servlet/Login.java
+++ src/main/java/servlet/Login.java
@@ -3,7 +3,8 @@ package servlet;
 import dao.UsuarioJpaController;
 import dto.Usuario;
 import java.io.IOException;
-import static java.lang.System.out;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -16,30 +17,38 @@ public class Login extends HttpServlet {
 
     UsuarioJpaController usuDAO = new UsuarioJpaController();
 
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try ( PrintWriter out = response.getWriter()) {
+            String user = request.getParameter("user");
+            String pass = request.getParameter("pass");
+            Usuario usu = new Usuario(user, pass);
+            Usuario b = usuDAO.validarUsuario(usu);
+            System.out.println(b);
+            if (b != null) {
+                HttpSession sesion = request.getSession();
+                sesion.setAttribute("usuario", user);
+                String token = utils.JwtUtil.generarToken(user);
+                // Respuesta en formato JSON
+                out.println("{\"resultado\":\"ok\",\"token\":\"" + token + "\"}");
+            } else {
+                out.println("{\"resultado\": \"errorrrrr\"}");
+            }
+        }
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
+        processRequest(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        String user = request.getParameter("user");
-        String pass = request.getParameter("pass");
-        Usuario usu = new Usuario(user, pass);
-        Usuario b = usuDAO.validarUsuario(usu);
-        System.out.println(b);
-        if (b != null) {
-            HttpSession sesion = request.getSession();
-            sesion.setAttribute("usuario", user);
-            String token = utils.JwtUtil.generarToken(user);
-            // Respuesta en formato JSON
-            out.println("{\"resultado\":\"ok\",\"token\":\"" + token + "\"}");
-        } else {
-            out.println("{\"resultado\": \"errorrrrr\"}");
-        }
+        processRequest(request, response);
+
     }
 
     @Override
